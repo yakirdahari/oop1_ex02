aba835a83412a2d7a20b3880e7bd9d461d710db1
yakirdahari <yakir242@gmail.com>
2022-11-27 19:49:40 +0200

    start

--- out/build/x64-Debug/ZipBackup.cmake
+++
@@ -1,5 +0,0 @@
-string (TIMESTAMP MY_TIMESTAMP "%Y-%m-%d-%H-%M-%S")
-execute_process (COMMAND D:/Visual Studio 2022/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe --build "C:/Users/Yakir/Desktop/oop1_ex02- Yakir_Dahari/out/build/x64-Debug" --target package_source)
-execute_process (COMMAND D:/Visual Studio 2022/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe -E copy "C:/Users/Yakir/Desktop/oop1_ex02- Yakir_Dahari/out/build/x64-Debug/oop1_ex02-yakir_dahari.zip" "C:/Users/Yakir/Desktop/oop1_ex02-yakir_dahari-${MY_TIMESTAMP}.zip")
-execute_process (COMMAND D:/Visual Studio 2022/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe -E echo "")
-execute_process (COMMAND D:/Visual Studio 2022/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe -E echo "***" C:/Users/Yakir/Desktop/oop1_ex02-yakir_dahari-${MY_TIMESTAMP}.zip "file created ***")
